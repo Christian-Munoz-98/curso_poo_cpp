3f924f946aa046eb115ef4a181d950665406ea7e
Christian_Windows_Desktop <chgeovany.15@gmail.com>
2022-01-15 00:01:42 -0600

    Encapsulación

--- u4l1-encapsulacion/main.cpp
+++ u4l1-encapsulacion/main.cpp
@@ -4,19 +4,20 @@
 using namespace std;
 
 class Persona {
-public:
+    
+private:
     string nombre;
     int edad;
 
-    Persona(string n, int e)
-    {
-        nombre = n;
-        edad = e;
-    }
+public:
+
+    Persona(string n, int e) : nombre(n), edad(e) {}
+
     ~Persona()
     {
         cout << "destructor" << endl;
     }
+
     void saludar()
     {
         cout << "Hola me llamo " << nombre << " y tengo "<< edad << " anos" << endl;
@@ -26,9 +27,7 @@ public:
 int main()
 {
     Persona *p = new Persona("Diana",26);
-    Persona *p2 = new Persona("Ximena",23); 
-
-    //delete p2;
+    Persona *p2 = new Persona("Ximena",23);
 
     p->saludar();
     p2->saludar();
